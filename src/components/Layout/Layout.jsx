b551286207879bd70df75a10f6e619ecb0e07712
petra <pk119123344@fer.hr>
2022-01-08 19:14:24 +0100

    Revert "Merge branch 'develop' into style-changes"
    This reverts commit a41a7af80d896f67728b3443a544862f7f6c30bd.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,9 +2,12 @@ import { useContext } from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import logo from '../../assets/logo2.png'
 import AuthContext from '../../store/auth-context'
+import RoomContext from '../../store/room-context'
+import { urls } from '../../utils/baseUrls'
 
 const Layout = ({ children }) => {
   const authCtx = useContext(AuthContext)
+  const roomCtx = useContext(RoomContext)
   const logoutHandler = () => authCtx.logout()
   const isLoggedIn = authCtx.isLoggedIn
   // const isInRoom = roomCtx.apartOfTheGame
@@ -15,40 +18,42 @@ const Layout = ({ children }) => {
 
   return (
     <div>
-      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+      <Navbar bg='dark' variant='dark'>
         <Container>
           <Navbar.Brand href='/'>
             <img src={logo} width='auto' height='30' className='d-inline-block align-top' alt='' />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-          <Navbar.Collapse id='responsive-navbar-nav'>
-            <Nav className='me-auto'>
-              <Nav.Link href='/'>Home</Nav.Link>
-              {isLoggedIn && (
-                <>
-                  <Nav.Link href='/leaderboard'>Leaderboard</Nav.Link>
-                  <Nav.Link href='/rooms'>Join room</Nav.Link>
-                </>
-              )}
-            </Nav>
-            <Nav>
-              {isLoggedIn && (
-                <Nav.Link style={{ paddingRight: '2em', color: '' }}>
-                  {user.firstName.toLocaleUpperCase()} {user.lastName.toLocaleUpperCase()}
+          <Nav className='me-auto'>
+            <Nav.Link href='/'>Home</Nav.Link>
+            {isLoggedIn && (
+              <>
+                <Nav.Link href='/leaderboard'>Leaderboard</Nav.Link>
+                <Nav.Link href='/rooms'>Join room</Nav.Link>
+
+                <Nav.Link onClick={() => logoutHandler()} style={{ paddingLeft: '45em' }}>
+                  Log out
                 </Nav.Link>
-              )}
-              {!isLoggedIn && (
-                <>
-                  <Nav.Link href='/sign-in'>Log in</Nav.Link>
-                  <Nav.Link href='/sign-up'>Sign up</Nav.Link>
-                </>
-              )}
-              {isLoggedIn && <Nav.Link onClick={() => logoutHandler()}>Log out</Nav.Link>}
-            </Nav>
-          </Navbar.Collapse>
+                {isLoggedIn && user && (
+                  <>
+                    <Nav.Link>
+                      {user.firstName} {user.lastName}
+                    </Nav.Link>
+                    {/* <Nav.Link>Score: {!user.score ? '0' : user.score}</Nav.Link> */}
+                  </>
+                )}
+              </>
+            )}
+            {!isLoggedIn && (
+              <>
+                <Nav.Link href='/sign-in' style={{ paddingLeft: '45em' }}>
+                  Log in
+                </Nav.Link>
+                <Nav.Link href='/sign-up'>Sign up</Nav.Link>
+              </>
+            )}
+          </Nav>
         </Container>
       </Navbar>
-
       <div>{children}</div>
     </div>
   )
