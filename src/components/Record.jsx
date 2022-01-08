a943a2e53e77612d8afd9b6f225fadc14a5577d3
petra <pk119123344@fer.hr>
2022-01-08 19:13:48 +0100

    added countdown before recording

--- src/components/Record.jsx
+++ src/components/Record.jsx
@@ -1,37 +1,58 @@
-import { useState } from "react"
-import { Alert, Col, Container, Row, Spinner } from "react-bootstrap"
-import { useRandomSign } from "../utils/http"
-import { RecordingComponent } from "./Recording/RecordingComponent"
-import TimerComponent from "./Timer/TimerComponent"
+import { useState } from 'react'
+import { Alert, Card, Col, Container, Row, Spinner, Stack } from 'react-bootstrap'
+import { useRandomSign } from '../utils/http'
+import { RecordingComponent } from './Recording/RecordingComponent'
+import TimerComponent from './Timer/TimerComponent'
 
-export const Record = ({socket}) => {
+export const Record = ({ socket }) => {
   const [recording, setRecording] = useState(true)
   const { data, loading, error } = useRandomSign()
+  const [countTimeToRecord, setCountTimeToRecord] = useState(true)
 
-  if(!!loading || !data) {
-    return <Spinner/>
+  if (!!loading || !data) {
+    return <Spinner />
   }
 
-  if(!!error) {
+  if (!!error) {
     return <div>ERROR</div>
   }
 
   console.log(data)
 
   return (
-    <Container style={{ width: "100%", paddingTop: "1em", minHeight: "40em", height: "auto", paddingBottom: "2em", background: "rgb(128, 204, 255, 0.3)" }} className="justify-content-md-center">
-      <Row>
-        <Alert variant={"Recording"} style={{ textAlign: "center" }}>
-          <TimerComponent timerInit={"00:00:03"} setRecording={setRecording}/>
-        </Alert>
-      </Row>
-      
-      <Container>
-            <Row style={{ textAlign: "center" }}><h4>Sign: {data.name}</h4><hr/></Row>
-            <Row>
-                <Col><RecordingComponent sign={data.name} socket={socket} recording={recording}/></Col>
+    <Container style={{ width: '100%', minHeight: '40em', height: 'auto', paddingBottom: '2em' }}>
+      {!countTimeToRecord && (
+        <Stack>
+          <Row>
+            <Alert variant={'Recording'} style={{ textAlign: 'center' }}>
+              <TimerComponent timerInit={'00:00:03'} setRecording={setRecording} />
+            </Alert>
+          </Row>
+
+          <Container>
+            <Row style={{ textAlign: 'center' }}>
+              <h4>Sign: {data.name}</h4>
+              <hr />
             </Row>
-        </Container>
+            <Row>
+              <Col>
+                <RecordingComponent sign={data.name} socket={socket} recording={recording} />
+              </Col>
+            </Row>
+          </Container>
+        </Stack>
+      )}
+      {countTimeToRecord && (
+        <Card body style={{ marginTop: '20%', justifyContent: 'flex-end' }}>
+          <Stack  className="mx-auto" style={{ justifyContent: 'flex-end'}} direction='horizontal' gap={2}>
+            <h4  className="me-auto"> You're first in row, we will start recording you in:</h4>
+            <TimerComponent style={{
+               float: 'right',
+               alignSelf: 'flex-end'
+            }} setRecording={setCountTimeToRecord} timerInit={'00:00:05'} />
+          </Stack>
+        </Card>
+      )}
     </Container>
   )
 }
