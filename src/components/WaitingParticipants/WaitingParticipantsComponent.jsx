2aa19da19cd2f1b756573dfbcf626bfb90fd827e
zdravko.pandzic <zdravko.pandzic@protel.net>
2022-01-08 17:43:48 +0100

    waiting room table css

--- src/components/WaitingParticipants/WaitingParticipantsComponent.jsx
+++ src/components/WaitingParticipants/WaitingParticipantsComponent.jsx
@@ -1,49 +1,63 @@
-import { useEffect, useState } from "react";
-import { baseUrl, urls } from "../../utils/baseUrls";
-import { Table } from "react-bootstrap";
-import axios from "axios";
-import { useParams } from "react-router";
+import { useEffect, useState } from 'react'
+import { baseUrl, urls } from '../../utils/baseUrls'
+import { Table } from 'react-bootstrap'
+import axios from 'axios'
+import { useParams } from 'react-router'
 
 const WaitingParticipantsComponent = () => {
-    const [users, setUsers] = useState([]);
-    const [myInterval, setMyInterval] = useState()
-    const { id } = useParams()
-    const getUsers = () => {
-    axios.get(baseUrl + urls.roomParticipantUrl + "/" + id)
-        .then((response) => {
-            const myUser = response.data;
-            setUsers(myUser);
-        })
-    }
-    
-    useEffect(() => {
-        setMyInterval(setInterval(() => {
-            getUsers()
-        }, 2000))
-        return () => {
-            clearInterval(myInterval)
-        }
-    },[]);
+  const [users, setUsers] = useState([])
+  const [myInterval, setMyInterval] = useState()
+  const { id } = useParams()
+  const getUsers = () => {
+    axios.get(baseUrl + urls.roomParticipantUrl + '/' + id).then((response) => {
+      const myUser = response.data
+      setUsers(myUser)
+    })
+  }
 
-    return(
-      <Table variant="dark" striped bordered hover style={{width:"30em"}} responsive="true">
-          <thead>
-              <tr>
-                  <th>Name</th>
-              </tr>
-          </thead>
-          <tbody>
-              {users.map((user, i ) => {
-                  return(
-                    <tr>
-                        <td> {user.user.firstName}  {user.user.lastName} </td>
+  useEffect(() => {
+    setMyInterval(
+      setInterval(() => {
+        getUsers()
+      }, 2000),
+    )
+    return () => {
+      clearInterval(myInterval)
+    }
+  }, [])
+
+  return (
+    <div class='row'>
+      <div class='col'>
+        <div class='card'>
+          <div class='card-body'>
+            <table class='table'>
+              <thead>
+                <tr>
+                  <th scope='col'>First name</th>
+                  <th scope='col'>Last name</th>
+                  <th scope='col'>Email</th>
+                  <th scope='col'>Score</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user, i) => {
+                  return (
+                    <tr key={Math.random()}>
+                      <td>{user.user.firstName}</td>
+                      <td>{user.user.lastName}</td>
+                      <td>{user.user.email}</td>
+                      <td>{user.user.score}</td>
                     </tr>
                   )
-              })}
-          </tbody>
-      </Table>
-    )
-    
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default WaitingParticipantsComponent;
+export default WaitingParticipantsComponent
