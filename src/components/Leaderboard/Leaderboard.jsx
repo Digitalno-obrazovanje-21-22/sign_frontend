fad442f9953662b57f99d59087018b1b68a465c6
Barbara <bb49992@fer.hr>
2022-01-12 17:17:34 +0100

    Align and resize leaderboard

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -1,36 +1,38 @@
 const Leaderboard = ({ users }) => {
   return (
-    <div className='row'>
-      <div className='col'>
-        <div className='card'>
-          <div className='card-body'>
-            <table className='table'>
-              <thead>
-                <tr>
-                  <th scope='col'>#</th>
-                  <th scope='col'>Username</th>
-                  <th scope='col'>Score</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user) => {
-                  const totalScore = user.roomParticipants.reduce((acc, val) => acc+val.score, 0)
-                  return {...user, totalScore}
-                }).sort((a, b) => b.totalScore-a.totalScore).map((user, index) => (
-                  <tr key={index}>
-                    <td>{index+1}</td>
-                    <td>
-                      {user.firstName} {user.lastName}
-                    </td>
-                    <td>{user.totalScore}</td>
+    <div style={{justifyContent:"center", display:"flex"}}>
+      <div className='row' style={{ width: "65%", textAlign: "center" }}>
+        <div className='col'>
+          <div className='card'>
+            <div className='card-body'>
+              <table className='table'>
+                <thead>
+                  <tr>
+                    <th scope='col'>Rank</th>
+                    <th scope='col'>Username</th>
+                    <th scope='col'>Score</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div> 
+                </thead>
+                <tbody>
+                  {users.map((user) => {
+                    const totalScore = user.roomParticipants.reduce((acc, val) => acc + val.score, 0)
+                    return { ...user, totalScore }
+                  }).sort((a, b) => b.totalScore - a.totalScore).map((user, index) => (
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      <td>
+                        {user.firstName} {user.lastName}
+                      </td>
+                      <td>{user.totalScore}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
-    </div> 
+    </div>
   )
 }
 export default Leaderboard
