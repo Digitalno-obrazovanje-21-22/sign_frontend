7f8fd63bf39e5575683415dad4cace741f021292
Barbara <bb49992@fer.hr>
2022-01-17 20:45:03 +0100

    Fix guessing component, add alert and buttons

--- src/components/GuessingSign/GuessingComponent.jsx
+++ src/components/GuessingSign/GuessingComponent.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Alert, Toast, ToastHeader, ToastBody } from "react-bootstrap";
+import { Container, Row, Alert, Toast, ToastHeader, ToastBody, Button, ListGroup } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import TimerComponent from "../Timer/TimerComponent";
 import videoPaths from '../../assets/videoPaths'
@@ -6,12 +6,12 @@ const GuessingComponent = ({ videoUrl, socket, token, sign, roomId }) => {
     const [guessing, setGuessing] = useState(true)
     const [selectedOption, setSelectedOption] = useState(null);
     const [video, setVideo] = useState(null)
-    const [options] = useState([sign, ...videoPaths.filter(val => val.name!=sign).map(val => val.name).slice(0,3)])
+    const [options] = useState([sign, ...videoPaths.filter(val => val.name != sign).map(val => val.name).slice(0, 3)])
 
     useEffect(() => {
-        if(!guessing) {
+        if (!guessing) {
             setTimeout(() => {
-                socket.emit("setPoints", {token, roomId, guessed: selectedOption === sign, sign })
+                socket.emit("setPoints", { token, roomId, guessed: selectedOption === sign, sign })
             }, 2000)
         }
     }, [guessing])
@@ -22,47 +22,40 @@ const GuessingComponent = ({ videoUrl, socket, token, sign, roomId }) => {
 
     return (
         <Container >
-            <Row>
-                <Alert variant={"Recording"} style={{ textAlign: "center" }}>
-                    <TimerComponent timerInit={"00:00:10"} setRecording={setGuessing}/>
-                </Alert>
-            </Row>
-            <Row style={{ textAlign: "center" }}><h4>{guessing ? "Guess the sign!" : "Guessing finished!"}</h4></Row><hr />
-            <Row>
-                {!!video && <video src={URL.createObjectURL(video)} width={520} height={480} autoPlay playsInline loop muted/>}
+            <Row style={{ textAlign: "center", marginTop:"1em" }}><h4>{guessing ? "Guess the sign!" : "Guessing finished!"}</h4></Row>
+
+                {guessing &&
+                    <Alert variant={"Recording"} style={{ textAlign: "center" }}>
+                        <TimerComponent timerInit={"00:00:10"} setRecording={setGuessing} />
+                    </Alert>}
+                {!guessing &&
+                    <Alert variant={selectedOption === sign ? "success" : "danger"} style={{ textAlign: "center" }}>
+                        {selectedOption == sign ? "Good job! Correct answer is " + sign : "Your answer is incorrect. Correct answer is: " + sign}
+                    </Alert>}
+            <Row>  <hr />
+                {!!video && <video src={URL.createObjectURL(video)} width={520} height={480} autoPlay playsInline loop muted />}
             </Row>
             <Row>
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <form >
-                    {options.map((option, i) => {
-                                    return (
-                                        <div className="radio">
-                                            <label>
-                                                <input
-                                                    type="radio"
-                                                    value={option}
-                                                    checked={selectedOption === option}
-                                                    onChange={(e) => setSelectedOption(e.target.value)}
-                                                />
-                                                {option}
-                                            </label>
-                                        </div>
-                                    )
-                                })}
-                    </form>
-                    <Toast show={!guessing}>
-                        <ToastHeader>
-                        {selectedOption == sign ? "Correct" : "Incorrect"}
-                        </ToastHeader>
-                        <ToastBody>
-                            {selectedOption == sign ? "Good job! Correct answer is " + sign : "Your answer is incorrect. Correct answer is: " + sign}
-                        </ToastBody>
-                    </Toast>
-                    </div>
 
-                    </Row>
+                    <ListGroup>
+                        <Row>
+                            {options.map((option, i) => {
+                                return (
+                                    <>
+                                        <div className="col-sm-6" onClick={() => setSelectedOption(option)}>
+                                            <Button style={{ backgroundColor: selectedOption === option ? 'rgb(13,110,253)' : 'rgb(13,110,253,0.7)', width: "100%", marginTop: "1em", marginRight: "1em" }} value={option} active={option === selectedOption}>{option}</Button>
+                                        </div></>
+                                )
+                            })}
+                        </Row>
+                    </ListGroup>
 
-                </Container>
+                </div>
+
+            </Row>
+
+        </Container>
     )
 }
 
