cd2a655f6e88decac3c473be543d89c4e7fdea2c
Barbara <bb49992@fer.hr>
2022-01-19 17:13:17 +0100

    remove random text from homePage

--- src/components/StartingPage/StartingPageContent.jsx
+++ src/components/StartingPage/StartingPageContent.jsx
@@ -71,7 +71,6 @@ const StartingPageContent = () => {
               <Card.Body>
                 <Card.Title>{activeVideo.name}</Card.Title>
                 <Card.Text>Video path: {activeVideo.videoUrl}</Card.Text>
-                <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
               </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
